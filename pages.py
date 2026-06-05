5ccc197f097754b8fda81e3a373f4025ff749993
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-05 17:41:47 +0300

    Update pages.py

--- pages.py
+++ pages.py
@@ -11,3 +11,16 @@ def profitability_page():
 def category_page():
 
 def report_page()
+
+def render_page(data):
+
+    selected = st.session_state["page"]
+
+    if selected == "stok":
+        stock_page()
+
+    elif selected == "miad":
+        miad_page()
+
+    elif selected == "rapor":
+        report_page()
