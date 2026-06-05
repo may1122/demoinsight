8c4ceed5a4ebd488c8a04ecfd3dd734f67272998
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-05 17:39:50 +0300

    Create theme.py

---
+++ theme.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def load_theme():
+
+    st.markdown("""
+    <style>
+
+    .metric-card{
+        border-radius:20px;
+    }
+
+    </style>
+    """, unsafe_allow_html=True)
