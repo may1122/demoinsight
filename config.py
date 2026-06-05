63db1a8fe35e34001caa3d97af40cc1b54867d25
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-05 17:39:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+DEMO_USERS = {
+    "basic": {
+        "password": "basic2026",
+        "membership": "Basic"
+    },
+    "premium": {
+        "password": "premium2026",
+        "membership": "Premium"
+    }
+}
+
+CRITICAL_DAYS = 5
+WARNING_DAYS = 15
+MIAD_DAYS = 90
+DEAD_STOCK_DAYS = 90
