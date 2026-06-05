98400915e82cb779f59524657b324e647698d122
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-05 17:40:50 +0300

    Create analytics.py

---
+++ analytics.py
@@ -0,0 +1,23 @@
+def calculate_score(df):
+
+    return 82
+
+
+def calculate_dead_stock(df):
+
+    return dead_stock_df
+
+
+def calculate_miad(df):
+
+    return miad_df
+
+
+def calculate_orders(df):
+
+    return order_df
+
+
+def calculate_lost_profit(df):
+
+    return result
